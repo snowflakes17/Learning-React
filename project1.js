3130886bfac320165900c8a51bd05ceabaed53ed
Shikha_1708 <97460961+snowflakes17@users.noreply.github.com>
2023-02-12 23:20:12 +0530

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,18 @@
+function MainContent() {
+    return (
+        <h1>I'm learning React!</h1>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <Navbar />
+        <MainContent />
+    </div>,
+    document.getElementById("root")
+)
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is an imperative way to program"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
