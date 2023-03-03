068d89927e3c705a86c3f0251e2801a28f3fdb3d
Shivani <shivani14306@gmail.com>
2023-03-03 22:15:55 +0530

    first commit

---
+++ mini_project/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import navbar from "./components/navbar"
+import main from "./components/main"
+
+export default function App() {
+    return (
+        <div className="container">
+            <navbar />
+            <main />
+        </div>
+    )
+}
---
+++ mini_project/components/navbar.js
@@ -0,0 +1,27 @@
+/**
+Challenge: Complete the Navbar to match the design
+
+Hint: use the Figma file for the most accurate peek at the design
+(colors, sizes, fonts, etc.)
+*/
+
+
+
+
+
+import React from "react"
+
+export default function Navbar() {
+    return (
+
+        <nav>
+            <img src="../image/react_logo.jpg" className="nav--icon" />
+            <h3 className="nav--logo_text">React Facts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
+
+
+
+
